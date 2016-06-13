f869853fb7d589ecc15df40779afcf5d3caa72fa
Paul van Schayck <polleke@gmail.com>
2016-06-13 16:42:24 +0200

    Switch from typeahead to select2

--- js/autocomplete_field_config.js
+++ js/autocomplete_field_config.js
@@ -10,88 +10,73 @@ Drupal.behaviors.islandora_ontology_autocomplete = {
             var type = '';
 
             jQuery(container, context).once('processed', function () {
-                jQuery(container).typeahead({
-                        minLength: 3,
-                        highlight: true
+                jQuery(container).select2({
+                    placeholder: 'Select an ontology term',
+                    ajax: {
+                        url: relativePath + 'api/select',
+                        dataType: 'json',
+                        delay: 250,
+                        data: function (params) {
+                            return {
+                                q: params.term === undefined ? '*' : params.term, // search term
+                                start: params.page === undefined ? 0 : params.page * 10,
+                                ontology: ontology
+                            };
+                        },
+                        processResults: selectResponse,
+                        cache: true
                     },
-                    {
-                        name: 'selection',
-                        source: getSelectHound(relativePath, ontology, type),
-                        display: 'value',
-                        limit: 20,
-                        templates: getSuggestTemplate()
-                    })
-                    .on('typeahead:selected', {'name': name}, termSelected);
+                    templateResult: getSuggestTemplate,
+                    // This nextSearchTerm does not work:
+                    // http://stackoverflow.com/questions/35976322/whats-the-select2-v4-equivalent-of-v3-5s-nextsearchterm
+                    nextSearchTerm: function (selectedObject, currentSearchTerm) {
+                        return currentSearchTerm;
+                    }
+                }).on('select2:select', {'name': name}, termSelected);
 
             })
         });
 
-        function termSelected(eventObject, suggestionObject, suggestionDataset) {
-            jQuery("input[name='" + eventObject.data.name + "[ontologyLabel]']").val(suggestionObject.value);
-            jQuery("input[name='" + eventObject.data.name + "[ontologyId]']").val(suggestionObject.id);
+        function termSelected(eventObject) {
+            var obj = jQuery(eventObject.currentTarget).select2("data")[0];
+            jQuery("input[name='" + eventObject.data.name + "[ontologyLabel]']").val(obj.text);
+            jQuery("input[name='" + eventObject.data.name + "[ontologyId]']").val(obj.data.iri);
         }
 
-        function getSelectHound (relativePath, ontology, type) {
-
-            var ontologyParam ='';
-            if (ontology) {
-                ontologyParam = '&ontology=' + ontology;
+        function getSuggestTemplate (suggestion) {
+            if (!suggestion.id) {
+                return suggestion.text;
             }
-            if (type) {
-                ontologyParam += '&type=' + type;
+
+            var label = suggestion.data.label ;
+
+            var extra = "";
+            if (suggestion.data.synonym != "") {
+                label =  suggestion.data.synonym;
+                extra = "<div class='sub-text'>synonym for " + suggestion.text + "</div>"
             }
-            return new Bloodhound({
-                datumTokenizer: Bloodhound.tokenizers.whitespace,
-                queryTokenizer: Bloodhound.tokenizers.whitespace,
-                identify: function(obj) { return obj.id; },
-                remote: {
-                    url: relativePath + 'api/select?rows=21&q=%QUERY' + ontologyParam,
-                    wildcard: "%QUERY",
-                    transform: function (response) {
-                        return selectResponse(response)
-                    }
-                }
-            });
 
+            var objectTypeHtml = "<div class='ontology-source'>" + suggestion.data.prefix + "</div>";
+                objectTypeHtml+="&nbsp;<div class='term-source'>" + suggestion.data.shortForm + "</div>";
+
+
+            return jQuery("" +
+                "<div style='width: 100%; display: table;'> <div style='display: table-row'><div  style='display: table-cell;' class='ontology-suggest'><div class='suggestion-value'>" + label + "</div>" + extra + "</div><div style='vertical-align:middle; text-align: right; display: table-cell;'>" + objectTypeHtml + "</div></div></div>"
+            );
         }
 
-        function getSuggestTemplate () {
+        function selectResponse (response, params) {
 
-            return {
-                suggestion: function(suggestion) {
-
-                    var label = suggestion.data.label ;
-
-                    var extra = "";
-                    if (suggestion.data.synonym != "") {
-                        label =  suggestion.data.synonym;
-                        extra = "<div class='sub-text'>synonym for " + suggestion.value + "</div>"
-                    }
-
-                    var objectTypeHtml = "<div class='ontology-source'>" + suggestion.data.prefix + "</div>"
-                    var type = getUrlType(suggestion.data.type);
-
-                    if (type != 'ontology') {
-                        objectTypeHtml+="&nbsp;<div class='term-source'>" + suggestion.data.shortForm + "</div>";
-
-                    }
-
-                    return "<div style='width: 95%; display: table;'> <div style='display: table-row'><div  style='display: table-cell;' class='ontology-suggest'><div class='suggestion-value'>" + label + "</div>" + extra + "</div><div style='vertical-align:middle; text-align: right; width:60px; display: table-cell;'>" + objectTypeHtml + "</div></div></div>";
-
-                    //Handlebars.compile('<div><strong>{{value}}</strong> – {{data.ontology}}</div>');
-
-                }
-            };
-        }
-
-        function selectResponse (response) {
             // Map the remote source JSON array to a JavaScript object array
-            var query = response.responseHeader.params.q;
-            return jQuery.map(response.response.docs, function (dataItem) {
+            var items = jQuery.map(response.response.docs, function (dataItem) {
 
                 var id =   dataItem.id;
 
                 var label = dataItem.label;
+
+                if ( dataItem.type == 'ontology' ) {
+                    return null;
+                }
 
                 var synonym = "";
                 var cantHighlight = true;
@@ -121,28 +106,27 @@ Drupal.behaviors.islandora_ontology_autocomplete = {
                 }
                 return {
                     id: id,
-                    value: dataItem.label,
-                    data: {ontology: dataItem.ontology_name, prefix: dataItem.ontology_prefix, iri : dataItem.iri, label: label,synonym: synonym, shortForm: shortId, type: dataItem.type},
-                    query: query
+                    text: dataItem.label,
+                    data: {
+                        ontology: dataItem.ontology_name,
+                        prefix: dataItem.ontology_prefix,
+                        iri : dataItem.iri,
+                        label: label,
+                        synonym: synonym,
+                        shortForm: shortId,
+                        type: dataItem.type
+                    }
                 };
             });
+
+            params.page = params.page || 0;
+
+            return {
+                results: items,
+                pagination: {
+                    more: (params.page * 10) < response.response.numFound
+                }
+            }
         }
-
-        function getUrlType (type) {
-            var urlType = 'terms';
-            if (type == 'property') {
-                urlType = 'properties';
-            }
-            else if (type == 'individual') {
-                urlType= 'individuals';
-            }
-            else if (type == 'ontology') {
-                urlType= 'ontology';
-            }
-            return urlType;
-        }
-
-
-
     }
 };
